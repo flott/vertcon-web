cda90add25b2f7252a9744b0d25b40b8e85b7f72
Fred <lott.fred@gmail.com>
2015-04-09 23:46:12 +0000

    fix popup

--- static/index.js
+++ static/index.js
@@ -11,7 +11,7 @@ var popup = L.popup();
 function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
-		.setContent("You clicked the map at " + e.latlng.toString())
+		.setContent(e.latlng.toString())
 		.openOn(map);
 		}
 
